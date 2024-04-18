79a261a395ae1060309d6521d19fa879dc9a8b1e
Erbait987 <erbikaiev@gmail.com>
2024-04-18 10:52:51 +0600

    add env to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,16 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_CONTENTFUL_SPACE_ID:
+      procces.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN:
+      process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+    NEXT_PUBLIC_ENTRY_ID: process.env.NEXT_PUBLIC_ENTRY_ID,
+    NEXT_PUBLIC_ENTRY_ID_SKI: process.env.NEXT_PUBLIC_ENTRY_ID_SKI,
+    NEXT_PUBLIC_ENTRY_ID_BIKE: process.env.NEXT_PUBLIC_ENTRY_ID_BIKE,
+    NEXT_PUBLIC_TELEGRAM_URL: process.env.NEXT_PUBLIC_TELEGRAM_URL,
+  },
 };
 
 export default nextConfig;
